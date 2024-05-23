c20d1ab6ffa6e5e69fa02665503492e25e768fd9
YllGajtani <123652630+Yllgajtani@users.noreply.github.com>
2024-05-23 16:57:40 +0200

    Add files via upload

---
+++ p5/script.js
@@ -0,0 +1,23 @@
+function setup(){
+createCanvas(400, 400)
+
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+line(215,102,143,67);
+fill(0, 38, 255);
+ellipse(146,120,50,50);
+ellipse(197,120,50,50);
+ellipse(246,120,50,50);
+ellipse(167,151,50,50);
+ellipse(202,157,50,50);
+ellipse(238,152,50,50);
+ellipse(175,185,50,50);
+ellipse(223,190,50,50);
+ellipse(195,214,50,50);
+
+
+strokeWeight(3);
+line(215,102,143,67);
+}
